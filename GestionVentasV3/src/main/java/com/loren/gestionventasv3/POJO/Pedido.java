8e8a2b8d3ff583208584e6617a5abb1e8cf61d14
Pablo <pabbenmar2@alu.edu.gva.es>
2022-04-01 13:30:54 +0200

    Subida clase pedido

---
+++ GestionVentasV3/src/main/java/com/loren/gestionventasv3/POJO/Pedido.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.loren.gestionventasv3.POJO;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class Pedido {
+   
+}
